56ba2aa96dd4380ed21dba50a9c79b2fd047fd42
Gerson Rocha <gersonrocha9@gmail.com>
2025-05-27 11:48:11 -0300

    feat: enhance Explore and Profile screens with centered text layout using Box component for improved visual presentation

--- app/(protected)/(tabs)/explore.tsx
+++ app/(protected)/(tabs)/explore.tsx
@@ -1,10 +1,13 @@
+import { Box } from '@/src/components/box'
 import { Screen } from '@/src/components/screen'
 import { Text } from '@/src/components/text'
 
 export default function ExploreScreen() {
   return (
     <Screen>
-      <Text>Explore</Text>
+      <Box justifyContent="center" alignItems="center" flex={1}>
+        <Text>Explore</Text>
+      </Box>
     </Screen>
   )
 }
--- app/(protected)/(tabs)/profile.tsx
+++ app/(protected)/(tabs)/profile.tsx
@@ -1,10 +1,13 @@
+import { Box } from '@/src/components/box'
 import { Screen } from '@/src/components/screen'
 import { Text } from '@/src/components/text'
 
 export default function ProfileScreen() {
   return (
     <Screen>
-      <Text>Profile</Text>
+      <Box justifyContent="center" alignItems="center" flex={1}>
+        <Text>Perfil</Text>
+      </Box>
     </Screen>
   )
 }
