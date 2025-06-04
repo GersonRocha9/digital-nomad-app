14db005ae632dfcaac43edabb5b7eee5095032cb
Gerson Rocha <gersonrocha9@gmail.com>
2025-06-04 17:47:42 -0300

    feat: enhance BottomSheetMap component with dynamic height and close button for improved user interaction

--- src/containers/bottom-sheet-map.tsx
+++ src/containers/bottom-sheet-map.tsx
@@ -1,6 +1,11 @@
+import { useWindowDimensions } from 'react-native'
+
 import MapView from 'react-native-maps'
 
 import { BottomSheet, type IBottomSheetProps } from '../components/bottom-sheet'
+import { Box } from '../components/box'
+import { IconButton } from '../components/icon-button'
+import { useAppTheme } from '../components/theme/useAppTheme'
 
 import type { ICity } from '../types'
 
@@ -12,20 +17,28 @@ export function BottomSheetMap({
   location,
   ...bottomSheetProps
 }: BottomSheetMapProps) {
+  const { height } = useWindowDimensions()
+  const { borderRadii, spacing } = useAppTheme()
+
   return (
     <BottomSheet {...bottomSheetProps}>
       <MapView
         style={{
           width: '100%',
-          height: 700,
+          height: height * 0.8,
+          borderRadius: borderRadii.default,
         }}
         initialRegion={{
           latitude: location.latitude,
           longitude: location.longitude,
           latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          longitudeDelta: 0.2,
         }}
       />
+
+      <Box position="absolute" right={spacing.padding} top={spacing.padding}>
+        <IconButton iconName="Close" onPress={bottomSheetProps.onPress} />
+      </Box>
     </BottomSheet>
   )
 }
