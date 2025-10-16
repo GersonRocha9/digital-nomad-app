4fc823b018538d5a040b5b8b820c85320b88d63e
Gerson Rocha <gersonrocha9@gmail.com>
2025-10-16 12:07:01 -0300

    refactor: enhance SignInScreen with logo and welcome text for improved user experience and visual appeal

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 
+import { Image } from 'react-native'
+
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { useAuthSignIn } from '@/src/domain/auth/operations/useAuthSignIn'
 import { Button } from '@/src/ui/components/button'
 import { Screen } from '@/src/ui/components/screen'
+import { Text } from '@/src/ui/components/text'
 import { TextInput } from '@/src/ui/components/text-input'
 
 export default function SignInScreen() {
@@ -19,6 +22,21 @@ export default function SignInScreen() {
   return (
     <Screen>
       <SafeAreaView>
+        <Image
+          source={require('../assets/images/logo.png')}
+          style={{
+            width: 150,
+            height: 60,
+            alignSelf: 'center',
+            marginTop: 20,
+            marginBottom: 60,
+          }}
+        />
+
+        <Text variant="title22" alignSelf="center" marginBottom="s16">
+          Bem-vindo
+        </Text>
+
         <TextInput
           label="E-mail"
           value={email}
@@ -28,7 +46,6 @@ export default function SignInScreen() {
         />
 
         <TextInput
-          errorMessage="Senha incorreta"
           label="Senha"
           value={password}
           onChangeText={setPassword}
@@ -37,8 +54,23 @@ export default function SignInScreen() {
           secureTextEntry
         />
 
-        <Button title="Entrar" onPress={handleSignIn} mb="s56" />
-        <Button title="Entrar" onPress={handleSignIn} variant="secondary" />
+        <Text
+          marginBottom="s16"
+          alignSelf="flex-end"
+          variant="text14"
+          color="primary"
+        >
+          Esqueceu sua senha?
+        </Text>
+
+        <Button title="Entrar" onPress={handleSignIn} />
+
+        <Text marginTop="s16" alignSelf="center" variant="text14" color="gray2">
+          Ainda não tem sua conta?{' '}
+          <Text color="primary" variant="title14">
+            Criar
+          </Text>
+        </Text>
       </SafeAreaView>
     </Screen>
   )
