097486382cbe7f03803ecf0b6f32cb7d337580aa
Gerson Rocha <gersonrocha9@gmail.com>
2025-10-19 15:27:26 -0300

    feat: enhance CityDetailsScreen with loading and error states, and add animations for improved user experience

--- app/(protected)/city-details/[id].tsx
+++ app/(protected)/city-details/[id].tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-import { Pressable } from 'react-native'
+import { ActivityIndicator, Pressable } from 'react-native'
 
 import { useLocalSearchParams } from 'expo-router'
-import { useSharedValue } from 'react-native-reanimated'
+import Animated, { FadeIn, useSharedValue } from 'react-native-reanimated'
 
 import { useCityFindById } from '@/src/domain/city/operations/useCityFindById'
 import { Divider } from '@/src/ui/components/divider'
 import { Screen } from '@/src/ui/components/screen'
 import { Text } from '@/src/ui/components/text'
+import { useAppTheme } from '@/src/ui/components/theme/useAppTheme'
 import { BottomSheetMap } from '@/src/ui/containers/bottom-sheet-map'
 import { CityDetailsHeader } from '@/src/ui/containers/city-details-header'
 import { CityDetailsInfo } from '@/src/ui/containers/city-details-info'
@@ -16,19 +17,30 @@ import { CityDetailsMaps } from '@/src/ui/containers/city-details-maps'
 import { CityDetailsRelatedCities } from '@/src/ui/containers/city-details-related-cities'
 import { CityDetailsTouristAttractions } from '@/src/ui/containers/city-details-tourist-attractions'
 
+const PAGE_ANIMATION_TIME = 1000
+
 export default function CityDetailsScreen() {
   const { id } = useLocalSearchParams<{ id: string }>()
-  const { data: city } = useCityFindById(id)
+  const { colors } = useAppTheme()
+  const { data: city, isLoading, error } = useCityFindById(id)
   const isOpenBottomSheet = useSharedValue(false)
 
   function toggleMapBottomSheet() {
     isOpenBottomSheet.value = !isOpenBottomSheet.value
   }
 
-  if (!city) {
+  if (isLoading) {
     return (
       <Screen flex={1} alignItems="center" justifyContent="center">
-        <Text>City not found</Text>
+        <ActivityIndicator color={colors.primary} />
+      </Screen>
+    )
+  }
+
+  if (error || !city) {
+    return (
+      <Screen flex={1} alignItems="center" justifyContent="center">
+        <Text>Erro ao buscar cidade</Text>
       </Screen>
     )
   }
@@ -36,41 +48,45 @@ export default function CityDetailsScreen() {
   return (
     <>
       <Screen style={{ paddingHorizontal: 0 }} scrollable>
-        <CityDetailsHeader
-          id={city.id}
-          coverImage={city.coverImage}
-          categories={city.categories}
-          isFavorite={city.isFavorite}
-        />
+        <Animated.View entering={FadeIn.duration(PAGE_ANIMATION_TIME)}>
+          <CityDetailsHeader
+            id={city.id}
+            coverImage={city.coverImage}
+            categories={city.categories}
+            isFavorite={city.isFavorite}
+          />
 
-        <CityDetailsInfo
-          name={city.name}
-          country={city.country}
-          description={city.description}
-        />
+          <CityDetailsInfo
+            name={city.name}
+            country={city.country}
+            description={city.description}
+          />
 
-        <Divider paddingHorizontal="padding" />
+          <Divider paddingHorizontal="padding" />
 
-        <CityDetailsTouristAttractions
-          touristAttractions={city.touristAttractions}
-        />
+          <CityDetailsTouristAttractions
+            touristAttractions={city.touristAttractions}
+          />
 
-        <Divider paddingHorizontal="padding" />
+          <Divider paddingHorizontal="padding" />
 
-        <Pressable onPress={toggleMapBottomSheet}>
-          <CityDetailsMaps location={city.location} />
-        </Pressable>
+          <Pressable onPress={toggleMapBottomSheet}>
+            <CityDetailsMaps location={city.location} />
+          </Pressable>
 
-        <Divider paddingHorizontal="padding" />
+          <Divider paddingHorizontal="padding" />
 
-        <CityDetailsRelatedCities id={city.id} />
+          <CityDetailsRelatedCities id={city.id} />
+        </Animated.View>
       </Screen>
 
-      <BottomSheetMap
-        location={city.location}
-        isOpen={isOpenBottomSheet}
-        onPress={toggleMapBottomSheet}
-      />
+      <Animated.View entering={FadeIn.duration(0).delay(PAGE_ANIMATION_TIME)}>
+        <BottomSheetMap
+          location={city.location}
+          isOpen={isOpenBottomSheet}
+          onPress={toggleMapBottomSheet}
+        />
+      </Animated.View>
     </>
   )
 }
